3d36a1a709e7851089c5836dd8a23c77348c9c8e
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-08-28 10:05:54 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -16,7 +16,7 @@ def get_args():
 
 
 def change_mac(interface, new_mac='00:11:22:33:44:55'):
-    print('[+] Changing Mac address for ' + interface + ' to ' + new_mac)
+    print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
 
     subprocess.call(['sudo', 'ifconfig', interface, 'down'])
     subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
