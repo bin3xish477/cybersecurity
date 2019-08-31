89eb57529b0baa704d1a39b8957ec5a6b0990f59
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-08-30 23:17:45 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_args():
@@ -16,12 +17,32 @@ def get_args():
 
 
 def change_mac(interface, new_mac='00:11:22:33:44:55'):
-    print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
+    print('[+] Changing MAC address for ' + interface + ' to ' + new_mac + '\n')
 
     subprocess.call(['sudo', 'ifconfig', interface, 'down'])
     subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
     subprocess.call(['sudo', 'ifconfig', interface, 'up'])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['sudo', 'ifconfig',  interface])
+    mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_result:
+        return mac_address_result.group(0)
+    else:
+        print("[-] Couldn\'t read MAC address. \n")
+
 options = get_args()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC : " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] Mac address was successfully altered to : " + str(current_mac))
+else:
+    print("[-] MAC address did not change")
+
+    
