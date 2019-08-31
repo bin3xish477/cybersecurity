e62bf3ddb70a7bbd940957e1427b5d4357f0bbab
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-08-31 09:22:55 -0400

    Create network_scanner.py

---
+++ network_scanner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+***********************************************************
+Title :       network_scanner.py
+Author :      Alexis Rodriguez
+Created on :  August 31th, 2019
+Description : Scans network.
+Purpose :     Uses the subprocess, optparse, and re modules
+              to call system commands, customize build
+              commands, and extract data using regular
+              expressions, ultimately providing all the 
+              information needed to altar a network 
+              interface's MAC address.
+Build with :  python mac_changer.py -i [interface] -m [MAC]
+***********************************************************
+"""
