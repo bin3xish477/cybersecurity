f1daf854714fac0cf8b166e1cee17e263959cf48
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-09-01 13:23:34 -0400

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -22,8 +22,10 @@ import optparse
 
 def command_line_args():
     parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Target IP/ IP range")
-    opts, arguments = parser.parse_args()
+    parser.add_option("-t", "--target", dest="ip", help="Target IP/ IP range")
+    (opts, arguments) = parser.parse_args()
+    if not opts.ip:
+        print("[-] Please provide an IP address / range")
     return opts
 
 
@@ -34,14 +36,16 @@ def scan(ip):
     answered_list = scapy.srp(arp_req_broad, timeout=1, verbose=False)[0]
 
     def print_results():
+        print("    IP\t\t\tMAC Address")
         for value in answered_list:
             client_dict = dict()
             client_dict["IP"] = value[1].psrc
             client_dict["MAC"] = value[1].hwsrc
-            print(client_dict)
-            print("--------------------------------------------")
+            print("[+] " + client_dict["IP"] + "\t\t" + client_dict["MAC"])
     print_results()
 
 
 options = command_line_args()
-scan(options.target)
+scan(options.ip)
+
+
