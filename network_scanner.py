0198f968973e329a8c7fde8fff85954909a6f94b
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-08-31 23:27:19 -0400

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 
-"""
-***********************************************************
-Title :       network_scanner.py
-Author :      Alexis Rodriguez
-Created on :  August 31th, 2019
-Description : Scans network.
-Purpose :     Uses the subprocess, optparse, and re modules
-              to call system commands, customize build
-              commands, and extract data using regular
-              expressions, ultimately providing all the 
-              information needed to altar a network 
-              interface's MAC address.
-Build with :  python mac_changer.py -i [interface] -m [MAC]
-***********************************************************
-"""
+# """
+# ***********************************************************
+# Title :       network_scanner.py
+# Author :      Alexis Rodriguez
+# Created on :  August 31th, 2019
+# Description : Scans network.
+# Purpose :     Uses the subprocess, optparse, and re modules
+#               to call system commands, customize build
+#               commands, and extract data using regular
+#               expressions, ultimately providing all the
+#               information needed to altar a network
+#               interface's MAC address.
+# Build with :  sudo python mac_changer.py -i [interface] 
+                 -m [MAC]
+# ***********************************************************
+# """
+
+import scapy.all as scapy
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_broad = broadcast/arp_request
+    answered_list, unanswered_list = scapy.srp(arp_req_broad, timeout=1)
+
+    for value in answered_list:
+        print("IP : " + value[1].psrc)
+        print("MAC : " + value[1].hwsrc)
+        print("------------------------------")
+
+
+scan("10.0.2.1/24")
+
