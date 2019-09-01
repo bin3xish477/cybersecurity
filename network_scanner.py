9c9dae4985933e4a4ba23a73c325855970e04a37
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2019-09-01 13:13:53 -0400

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -12,25 +12,36 @@ Purpose :     Uses the subprocess, optparse, and re modules
               expressions, ultimately providing all the
               information needed to altar a network
               interface's MAC address.
-Build with :  sudo python mac_changer.py -i [interface] 
-              -m [MAC]
+Build with :  sudo python network_scanner.py -t [target IP]
 ***********************************************************
 """
 
 import scapy.all as scapy
+import optparse
+
+
+def command_line_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP/ IP range")
+    opts, arguments = parser.parse_args()
+    return opts
 
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_req_broad = broadcast/arp_request
-    answered_list, unanswered_list = scapy.srp(arp_req_broad, timeout=1)
+    answered_list = scapy.srp(arp_req_broad, timeout=1, verbose=False)[0]
 
-    for value in answered_list:
-        print("IP : " + value[1].psrc)
-        print("MAC : " + value[1].hwsrc)
-        print("------------------------------")
+    def print_results():
+        for value in answered_list:
+            client_dict = dict()
+            client_dict["IP"] = value[1].psrc
+            client_dict["MAC"] = value[1].hwsrc
+            print(client_dict)
+            print("--------------------------------------------")
+    print_results()
 
 
-scan("10.0.2.1/24")
-
+options = command_line_args()
+scan(options.target)
