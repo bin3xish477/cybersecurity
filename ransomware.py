389a583cdf3789f7bb1167f4cec8ade45e356297
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2020-02-18 10:07:35 -0500

    Create ransomware.py

---
+++ ransomware.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+Name : Alexis Rodriguez
+Date : 2020-02-18
+'''
+
+from cryptography.fernet import Fernet
+import os
+
+def initiate():
+
+
+if __name__ == '__main__':
+  initiate()
