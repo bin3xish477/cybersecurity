39241f3190aebce31a424cfd416c63f95584d6c1
Alexis Rodriguez <rodriguez10011999>
2020-02-26 11:04:50 -0500

    adding Conv3ni3nt modules

---
+++ Conv3ni3nt/conv3ni3nt.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+'''
+----------------------------------------------------------------
+Author : Alexis Rodriguez
+Start date : 2020-02-26
+End date : 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Description : 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+----------------------------------------------------------------
+'''
+try:
+	from colored import fg, attr
+	import sys
+	import os
+	import time
+	import threading
+	import subprocess as subp
+	from pyfiglet import Figlet
+except ImportError:
+	print('%s [-] Error importing a module %s' % (fg(196), attr(0)))
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 		Conv3ni3nt Class
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+class Conv3ni3nt:
+	def __init__(self):
+		pass
+
+def prompt_tools():
+	print('%sEnter the tools you would like to use: e.g. nmap nikto dirb...%s' % (fg(226), attr(0)))
+	tools = input('cmd> ')
+	lst_of_tools = tools.split()
+	return lst_of_tools
+
+def signature():
+	sig = Figlet(font='future')
+	print(sig.renderText('binexishatt'))
+	print('By : ' + '%sAlexis Rodriguez%s' % (fg(207), attr(0)))
+
+def initiate():
+	# print my signature
+	signature()
+	prompt_tools()
+	instance = Conv3ni3nt()
+	time.sleep(10)
+	os.system('clear')
+
+
+# %%%%%%%%%%%%%%%%
+# 		MAIN      
+# %%%%%%%%%%%%%%%%
+if __name__ == '__main__':
+	initiate()
