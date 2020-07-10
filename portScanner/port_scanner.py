31d9400fa06e757cc9a3d4bf68d9621d50177acd
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-07-09 22:13:27 -0400

    Update port_scanner.py

--- portScanner/port_scanner.py
+++ portScanner/port_scanner.py
@@ -7,10 +7,10 @@ try:
 except ImportError as err:
     print("Import error: {err}")
 
-def portScan(target_ip, start, finish):
+def portScan(target_ip, start, end):
     print(f'Scanning ports on target: \033[32m\033[01m{target_ip}\033[0m')
-    print(f'Port range: \033[95m\033[01m{start}-{finish}\033[0m\n')
-    for i in range(start, finish + 1):
+    print(f'Port range: \033[95m\033[01m{start}-{end}\033[0m\n')
+    for i in range(start, end + 1):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
             try:
                 soc.settimeout(3)
@@ -30,10 +30,10 @@ def portScan(target_ip, start, finish):
 
 def main():
     if len(argv) != 4:
-        print('\033[31m', 'Usage: ./port_scanner.py TARGET_IP START_PORT FINISH_PORT', '\033[0m')
+        print('\033[31m', 'Usage: ./port_scanner.py TARGET_IP START_PORT END_PORT', '\033[0m')
         exit(1)
-    TARGET, START, FINISH = argv[1], int(argv[2]), int(argv[3])
-    portScan(TARGET, START, FINISH)
+    TARGET, START, END = argv[1], int(argv[2]), int(argv[3])
+    portScan(TARGET, START, END)
 
 if __name__ == '__main__':
     try:
