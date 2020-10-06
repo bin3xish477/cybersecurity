c273a5ba268c38c979dd7924449be8803f515522
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-10-06 16:44:51 -0400

    updating repr

--- WindowsRegistry/change_wallpaper.py
+++ WindowsRegistry/change_wallpaper.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 
 from sys import argv
+from ctypes import (
+    windll,
+    byref,
+    c_long
+) 
 from winreg import (
     HKEY_CURRENT_USER,
     ConnectRegistry,
-    KEY_WRITE,
-    EnumValue
+    OpenKey,
+    KEY_ALL_ACCESS,
+    EnumValue,
+    SetValueEx,
+    QueryInfoKey,
+    REG_SZ
 )
 
 def change_wallpaper(image: str):
-   # specify None for first arg so winreg can find the default
-   # computer name, otherwise specify computer name
-   with ConnectRegistry(None, HKEY_CURRENT_USER) as hkey:
-        with 
-            # KEY_WRITE is basically like specifying "w" mode
-            OpenKey(hkey, r"Control Panel\Desktop", 0, KEY_WRITE)
+   with ConnectRegistry(None, HKEY_CURRENT_USER) as hkcu:
+        with \
+            OpenKey(hkcu, r"Control Panel\Desktop", 0, KEY_ALL_ACCESS) \
         as desktop_key:
-           for i in enumerate(range(1, 10)):
-               print(desktop_key.EnumValue(desktop_key, i))
+            print(f"[+] Changing wallpaper to {image} ...")
+            SetValueEx(desktop_key, "WallPaper", 0, REG_SZ, image)
+
+            # the following lines of code is boiler plate code
+            # for making telling Windows that registry settings have changed
+            # and that it needs to refresh
+            HWND_BROADCAST = 0xFFFF
+            WM_SETTINGCHANGE = 0x1A
+            SMTO_ABORTIFHUNG = 0x0002
+            result = c_long()
+            SendMessageTimeoutW = windll.user32.SendMessageTimeoutW
+            SendMessageTimeoutW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, u'Control Panel\\Desktop', SMTO_ABORTIFHUNG, 5000, byref(result),)
 
 if __name__ == "__main__":
     path_to_image: str = argv[1]
-    change_wallpaper()
+    change_wallpaper(path_to_image)
