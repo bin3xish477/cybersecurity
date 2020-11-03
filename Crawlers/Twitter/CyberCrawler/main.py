515ea5fd4c8c4b3f0363e5722e09bd108c4e7931
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-11-03 13:04:21 -0500

    Adding directory Crawlers/Twitter/CyberCrawler

---
+++ Crawlers/Twitter/CyberCrawler/main.py
@@ -0,0 +1,8 @@
+from twitter_crawler import TwitterCrawler
+from argparse import ArgumentParser
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Crawlers/Twitter/CyberCrawler/twitter_crawler.py
@@ -0,0 +1,24 @@
+from twitter import Api
+
+class TwitterCrawler:
+    
+    def __init__(
+        consumer_key, consumer_secret,
+        access_token_key, access_token_secret
+    ):
+        self.consumer_key=consumer_key
+        self.consumer_secret=consumer_secret
+        self.access_token_key=access_token_key
+        self.access_token_secret=access_token_secret
+        self.api=self.create_twitter_api()
+
+    def create_twitter_api(self):
+        return Api(
+            consumer_key=[self.consumer_key],
+            consumer_secret=[self.consumer_secret],
+            access_token_key=[self.access_token_key],
+            access_token_secret=[self.access_token_secret]
+        )
+
+    def search(self):
+        pass
