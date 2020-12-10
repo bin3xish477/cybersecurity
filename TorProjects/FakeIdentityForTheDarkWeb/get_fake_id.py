a3e92e1edc8509aac5eec9fa55293273dde43046
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-12-10 10:19:09 -0500

    Updating get_fake_id.py

--- TorProjects/FakeIdentityForTheDarkWeb/get_fake_id.py
+++ TorProjects/FakeIdentityForTheDarkWeb/get_fake_id.py
@@ -1,54 +1,84 @@
 #!/usr/bin/env python3
 from stem import Signal
 from stem.control import Controller
-from argparse import ArgumentParser
+from argparse import ArgumentParser as AP
 from fake_useragent import UserAgent
 from json import dump
 from csv import writer
 from requests import get
 from os import environ
 from time import sleep
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from colored import fg, attr
-from re import findall, IGNORECASE
+from re import compile, findall, IGNORECASE
+from prettytable import PrettyTable as PT
+from random import randint
 
 
 __author__ = "Alexis Rodriguez", "binexisHATT"
 __date__   = 12_09_2020
 
 
+row_data_cell_re = compile(r'<td [\w="]+>([\w\s\.@:()\'-0-9]+)<\/td>')
+
+
+def extract_row_data_cells(row: str):
+        return row_data_cell_re.findall(row, IGNORECASE)
+        
+
 def parse_response(resp: str):
-    html_parser = BeautifulSoup(resp.text, "html.parser")
+    html_parser = bs(resp.text, "html.parser")
     #print(html_parser.prettify())
+
     all_tables = html_parser.findAll("table")
-    general_attributes = all_tables[2]
+    general_details = all_tables[2]
     physical_appearance = all_tables[3]
     personality = all_tables[4]
     federal_taxpayer_id_nums = all_tables[5]
     
-    for row in general_attributes.findAll("tr"):
-        value = findall(
-            r'<td [\w="]+>([\w\s\.@:()\'-0-9]+)<\/td>', str(row),
-            IGNORECASE)
-        print(value)
+    general_details_tbl = PT()
+    general_details_tbl.field_names = [
+        "%sGeneral Details%s" % (fg(randint(1, 220)), attr(0)), 
+        "%sValues%s" % (fg(randint(1, 220)), attr(0))
+        ]
+    for row in general_details.findAll("tr"):
+        v = extract_row_data_cells(str(row))
+        if len(v) != 2: continue
+        else: general_details_tbl.add_row(v)
+    print(general_details_tbl)
 
+    physical_appearance_tbl = PT()
+    physical_appearance_tbl.field_names = [
+        "%sPhysical Appearance%s" % (fg(randint(1, 220)), attr(0)), 
+        "%sValues%s" % (fg(randint(1, 220)), attr(0))
+        ]
     for row in physical_appearance.findAll("tr"):
-        value = findall(
-            r'<td [\w="]+>([\w\s\.@:()\'-0-9]+)<\/td>', str(row),
-            IGNORECASE)
-        print(value)
+        v = extract_row_data_cells(str(row))
+        if len(v) != 2: continue
+        else: physical_appearance_tbl.add_row(v)
+    print(physical_appearance_tbl)
 
+    personality_tbl = PT()
+    personality_tbl.field_names = [
+        "%sPersonality Traits%s" % (fg(randint(1, 220)), attr(0)), 
+        "%sValues%s" % (fg(randint(1, 220)), attr(0))
+        ]
     for row in personality.findAll("tr"):
-        value = findall(
-            r'<td [\w="]+>([\w\s\.@:()\'-0-9]+)<\/td>', str(row),
-            IGNORECASE)
-        print(value)
+        v = extract_row_data_cells(str(row))
+        if len(v) != 2: continue
+        else: personality_tbl.add_row(v)
+    print(personality_tbl)
 
+    federal_taxpayer_tbl = PT()
+    federal_taxpayer_tbl.field_names = [
+        "%sUS Federal Taxpayer Id Number (Tin)%s" % (fg(randint(1, 220)), attr(0)), 
+        "%sTax ID%s" % (fg(randint(1, 220)), attr(0))
+        ]
     for row in federal_taxpayer_id_nums.findAll("tr"):
-        value = findall(
-            r'<td [\w="]+>([\w\s\.@:()\'-0-9]+)<\/td>', str(row),
-            IGNORECASE)
-        print(value)
+        v = extract_row_data_cells(str(row))
+        if len(v) != 2: continue
+        else: federal_taxpayer_tbl.add_row(v)
+    print(federal_taxpayer_tbl)
 
 
 def make_request(url: str, gender: str):
@@ -80,26 +110,21 @@ def make_request(url: str, gender: str):
 
 
 def create_new_tor_ip():
-    with Controller.from_port(port=9051) as c:
-        c.authenticate(password=environ["TOR_PASS"])
-        c.signal(Signal.NEWNYM)
-
-
-def write_to_file(file_name: str, file_type: str):
-    pass
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate(password=environ["TOR_PASS"])
+        controller.signal(Signal.NEWNYM)
 
     
 if __name__ == "__main__":
     hidden_service_url = "http://elfq2qefxx6dv3vy.onion/fakeid.php"
 
-    parser = ArgumentParser()
-    parser.add_argument("-g", "--gender",
+    parser = AP(description="Generate identity to browse the Dark Web")
+    parser.add_argument(
+        "-g", "--gender",
         choices=("female", "male", "random"),
         default="random",
-        help="Select the gender to generate a Fake Id for")
-    parser.add_argument("-f", "--outfile",
-        choices=("txt", "json", "csv"),
-        help="Specify file type to save fake identity to")  
+        help="Select the gender to generate a Fake Id for"
+        )
 
     args = parser.parse_args()
 
