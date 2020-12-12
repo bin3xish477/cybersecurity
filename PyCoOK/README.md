b83f6c497245d73da7013ad92d99a8cf9fee815c
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-12-12 14:30:40 -0500

    Updating repository

---
+++ PyCoOK/README.md
@@ -0,0 +1,7 @@
+# PyCoOK
+
+### Compiled with Nuitka
+
+```bash
+python3 -m nuitka --follow-imports --standalone pycook
+```
---
+++ PyCoOK/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "[+] Installing nuitka ..."
+apt-get update
+apt-get install -y nuitka
+echo "[ OK ]"
+
+echo "[+] Downloading pycook binary ..."
+echo "[ OK ]"
