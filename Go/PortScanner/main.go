b76ed1b8371f870486254b184bf066c2fed79411
binexis <binexis@localhost.localdomain>
2020-12-17 22:45:12 -0500

    Updating repo

---
+++ Go/PortScanner/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sort"
+)
+
+func worker(ports, results chan int) {
+	for p := range ports {
+		addr := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		results <- p
+	}
+}
+
+func main() {
+	ports, results := make(chan int, 100), make(chan int)
+	var openPorts []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for range results {
+		port := <-results
+		fmt.Println(port)
+		openPorts = append(openPorts, port)
+	}
+
+	close(ports)
+	close(results)
+
+	sort.Ints(openPorts)
+	for _, port := range openPorts {
+		fmt.Printf("%d [open]\n", port)
+	}
+}
