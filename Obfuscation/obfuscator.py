31f67b90c3e8f104d76121d35542b2bc6590d21b
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-12-21 03:30:38 -0500

    Adding Obfuscation directory

---
+++ Obfuscation/obfuscator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+This Python script will take a provided Python
+script and obfuscate it's contents
+
+Usage:
+    obfuscator.py -f test.py
+    obfuscator.py -f test.py -r 10
+    obfuscator.py -f test.py -o test.py.obf
+"""
+
+__author__ = "QWxleGlzIFJvZHJpZ3Vlego="
+__date__   = 12_21_2020
+
+
