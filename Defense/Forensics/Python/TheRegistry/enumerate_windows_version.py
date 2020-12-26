bca4c3ad06cdcb56549f24ce8a548b792b8cf7f0
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2020-12-25 20:22:55 -0500

    Update enumerate_windows_version.py

--- Defense/Forensics/Python/TheRegistry/enumerate_windows_version.py
+++ Defense/Forensics/Python/TheRegistry/enumerate_windows_version.py
@@ -1,11 +1,12 @@
 from winreg import (
-                    ConnectRegistry,
-                    OpenKey,
-                    KEY_ALL_ACCESS,
-                    EnumValue,
-                    QueryInfoKey,
-                    HKEY_LOCAL_MACHINE
-                    )
+  ConnectRegistry,
+  OpenKey,
+  KEY_ALL_ACCESS,
+  EnumValue,
+  QueryInfoKey,
+  HKEY_LOCAL_MACHINE,
+  HKEY_CURRENT_USER
+)
 if __name__ == "__main__":
     with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hive:
         with OpenKey(hive, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion", 0, KEY_ALL_ACCESS) as hosts_key:
