1a2544958072ef5da45cf95755ce95610f463b90
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-12-25 20:14:20 -0500

    Updating Defense dir

---
+++ Defense/Forensics/Python/TheRegistry/enumerate_windows_version.py
@@ -0,0 +1,20 @@
+from winreg import (
+                    ConnectRegistry,
+                    OpenKey,
+                    KEY_ALL_ACCESS,
+                    EnumValue,
+                    QueryInfoKey,
+                    HKEY_LOCAL_MACHINE
+                    )
+if __name__ == "__main__":
+    with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hive:
+        with OpenKey(hive, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion", 0, KEY_ALL_ACCESS) as hosts_key:
+            num_of_values = QueryInfoKey(hosts_key)[1]
+            for i in range(num_of_values):
+                values = EnumValue(hosts_key, i)
+                print(values)
+
+
+
+
+
--- Defense/Forensics/Python/TheRegistry/registry_forensics.py
+++ Defense/Forensics/Python/TheRegistry/registry_forensics.py
@@ -2,18 +2,32 @@ from winreg import (
                     ConnectRegistry,
                     OpenKey,
                     KEY_ALL_ACCESS,
-                    EnumKey,
                     EnumValue,
-                    QueryInfoKey
-                    HKEY_LOCAL_MACHINE
+                    QueryInfoKey,
+                    HKEY_LOCAL_MACHINE,
+                    HKEY_CURRENT_USER
                     )
+def enum_key(hive, subkey:str):
+    with OpenKey(hive, subkey, 0, KEY_ALL_ACCESS) as key:
+        num_of_values = QueryInfoKey(key)[1]
+        for i in range(num_of_values):
+            values = EnumValue(key, i)
+            if values[0] == "LangID": continue
+            print(*values[:-1], sep="\t")
 if __name__ == "__main__":
-    with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hive:
-        with OpenKey(hive, r"Comm\\Tcpip\\Hosts", 0, KEY_ALL_ACCESS) as hosts_key:
-            keys = EnumKey(hosts_key)
-            values = EnumValue(hosts_key)
-
-
-
-
-
+    # Connecting to the HKEY_LOCAL_MACHINE hive
+    with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hklm_hive:
+        print("\nCurrent Version/Build Info")
+        print("-"*50)
+        enum_key(hklm_hive, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion")
+        print("\nSystem Environment Variables")
+        print("-"*50)
+        enum_key(hklm_hive, r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment")
+        print("\nStartup Applications")
+        print("-"*50)
+        enum_key(hklm_hive, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run")
+    # Connecting to the HKEY_CURRENT_USER hive
+    with ConnectRegistry(None, HKEY_CURRENT_USER) as hkcu_hive:
+        print("\nPreviously rand applications")
+        print("-"*50)
+        enum_key(hkcu_hive, r"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache")
