e4cb6add9c1f0ccad9d57570e95eef15fadd8129
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-06 21:33:18 -0500

    Delete IfsAndPathSystemFunctionAbuse.c

--- AttacksOnSUIDAndEnvVariables/IfsAndPathSystemFunctionAbuse.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-// providing the absolute path to `system` function can still
-// be abused by altering both the PATH and IFS environment
-// variables
-int main() {
-  system("/usr/bin/cal");
-  return 0;
-}
