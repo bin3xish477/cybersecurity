14e37e1eb7f6ec7fab0793c08c61d6fa4408bbf2
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-06 21:33:24 -0500

    Delete PathSystemFunctionAbuse.c

--- AttacksOnSUIDAndEnvVariables/PathSystemFunctionAbuse.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-// overwriting the PATH variable will allow the attacker to perform 
-// search order hijacking attack and replace the "cal" program that
-// would normally run
-int main() {
-  system("cal");
-  return 0;
-}
