31d1018505afdc22dd9ef6d6013bfb3dbc79c6cd
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-06 21:33:02 -0500

    Delete AbusingLdPreload.c

--- AttacksOnSUIDAndEnvVariables/AbusingLdPreload.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// altering the LD_PRELOAD environment variable will
-// allow an attacker to change the libraries loaded 
-// by a program using external libraries
-int main() {
-  printf("Hello World!");
-  sleep(2);
-  return 0;
-}
