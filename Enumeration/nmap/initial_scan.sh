7ca46aca45a297fcde4aa483bb2e98b1752cad7f
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-01-09 18:59:22 -0500

    Updating repo

--- Enumeration/nmap/initial_scan.sh
+++ Enumeration/nmap/initial_scan.sh
@@ -5,7 +5,7 @@ TARGET=$1
 [[ ! -d nmap ]] && mkdir nmap
 
 # Perform port scan against all tcp ports
-nmap -Pn -p- -oN nmap/all_ports.txt $TARGET
+nmap -vv -Pn -p- -oN nmap/all_ports.txt $TARGET
 
 # Filter ports from all ports scan for only open TCP ports
 OPEN_PORTS=$(cat nmap/all_ports.txt | egrep -oE '([0-9]+)/tcp' | \
@@ -14,4 +14,4 @@ OPEN_PORTS=$(cat nmap/all_ports.txt | egrep -oE '([0-9]+)/tcp' | \
 
 # Pass extracted open ports into an nmap with service scan
 # and default scripts enabled
-nmap -sV -sC -p $OPEN_PORTS -oN nmap/service_enum.txt $TARGET
+nmap -vv -Pn -sV -sC -p $OPEN_PORTS -oN nmap/service_enum.txt $TARGET
