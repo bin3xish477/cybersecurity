985b77c3a31b939f717f2fe42869cb9c511fecd4
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-01-09 18:46:57 -0500

    Updating repo

--- Enumeration/nmap/initial_scan.sh
+++ Enumeration/nmap/initial_scan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$TARGET=$1
+TARGET=$1
 # Make nmap directory if it doesn't exist
 [[ ! -d nmap ]] && mkdir nmap
 
@@ -8,7 +8,7 @@ $TARGET=$1
 nmap -Pn -p- -oN nmap/all_ports.txt $TARGET
 
 # Filter ports from all ports scan for only open TCP ports
-$OPEN_PORTS=$(cat nmap/all_ports.txt | egrep -oE '([0-9]+)/tcp' | \
+OPEN_PORTS=$(cat nmap/all_ports.txt | egrep -oE '([0-9]+)/tcp' | \
     awk -F/ '{print $1}' | \
     tr "\n" "," | rev | cut -c 2- | rev)
 
