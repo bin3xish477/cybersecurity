30eec4499a6f0b0ebcf386362ad3877c1fb282e1
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-01-19 13:30:12 -0500

    deleting c4N4Re dir

--- Defense/Canaries/c4N4Re/c4N4Re.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from configparser import ConfigParser
-
-from canary_watcher import CanaryWatcher
-
-if __name__ == "__main__":
-    with CanaryWatcher(ConfigParser().read("config.ini")) as cw:
-        pass
