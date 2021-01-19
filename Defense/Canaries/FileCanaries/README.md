1db0cad280f2415fee1f9dbe72f7fb9c9f0d5cb5
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-01-18 20:27:05 -0500

    updating canary.py

---
+++ Defense/Canaries/FileCanaries/README.md
@@ -0,0 +1,28 @@
+# File Canary
+
+Get an email whenever a file you intentionally create to lure attackers gets opened. This should be installed as a cronjob (Linux) or scheduled task (Windows). Enjoy!
+
+### Usage
+
+Set the following environment variables:
+
+	- `EMAIL_ADDR` - gmail address
+	- `EMAIL_PASS` - gmail password (if you are using 2fa authentication, you need to create an app password to authenticate to gmail: [](https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882)
+
+```
+usage: canary.py [-h] FILE
+
+Basic file canary Python program
+
+positional arguments:
+  FILE        File to act as canary
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
+### Example
+
+```
+python3 canary.py secrets.txt
+```
--- Defense/Canaries/FileCanaries/canary.py
+++ Defense/Canaries/FileCanaries/canary.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
+
 from emailer import Emailer
+
 from os import environ
+from os import stat
+
+from os.path import exists
+from os.path import isfile
+from os.path import realpath
+
+from time import sleep
+
+from argparse import ArgumentParser
+
+def watch_canary(f):
+    initial_file_access_time = stat(f).st_atime
+    sleep(5)
+
+    while True:
+        file_access_time = stat(f).st_atime
+        if file_access_time != initial_file_access_time:
+            gmail_address = environ["GMAIL_ADDR"]
+            gmail_password  = environ["GMAIL_PASS"]
+
+            with Emailer("smtp.gmail.com", 465) as email:
+
+                if email.authenticate(gmail_address, gmail_password):
+                    print("{*} Authentication successful!")
+                else:
+                    print("{-} Authentication unsuccessful!")
+
+                subject = "[WARNING] Canary Triggered"
+                message = f"Someone accessed the canary file:\n\t {realpath(f)}"
+
+                if email.send(gmail_address, subject, message):
+                    print("{*} Successfully sent email!")
+                else:
+                    print("{*} Email could not be sent!")
+            break
+        sleep(5)
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description="Basic file canary Python program")
+    parser.add_argument("FILE", help="File to act as canary")
 
-    gmail_address = environ["GMAIL_ADDR"]
-    gmail_password  = environ["GMAIL_PASS"]
-
-    with Emailer("smtp.gmail.com", 465) as email:
-
-        if email.authenticate(gmail_address, gmail_password):
-            print("{*} Authentication successful!")
-        else:
-            print("{-} Authentication unsuccessful!")
-
-        if email.send(gmail_address, "Testing", "Test message"):
-            print("{*} Successfully sent email!")
-        else:
-            print("{*} Email could not be sent!")
-
+    args = parser.parse_args()
+    if exists(args.FILE) and isfile(args.FILE):
+        watch_canary(args.FILE)
+    else:
+        print(f"{{-}} {args.FILE} does not exist!")
 
