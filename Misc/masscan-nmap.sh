020dab6b17e69b110bd2707d08becaa98ede4c2b
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-05-02 20:15:47 -0400

    updating masscan-nmap script

--- Misc/masscan-nmap.sh
+++ Misc/masscan-nmap.sh
@@ -25,7 +25,7 @@ fi
 
 PORT_LIST="7,9,13,21-23,25-26,37,53,79-81,88,106,110-111,113,119,135,139,143-144,179,199,389,427,443-445,465,513-515,543-544,548,554,587,631,646,873,990,993,995,1025-1029,1110,1433,1720,1723,1755,1900,2000-2001,2049,2121,2717,3000,3128,3306,3389,3986,4899,5000,5009,5051,5060,5101,5190,5357,5432,5631,5666,5800,5900,6000-6001,6379,6646,7070,8000,8008-8009,8080-8081,8443,8888,9100,9999-10000,27017,32768,49152-49157"
 
-echo -e "\nStarting Masscan scan (\e[0;92mINFO\e[0m)"
+echo -e "Starting Masscan scan (\e[0;92mINFO\e[0m)"
 # if you don't specify an adapter, packets distributed over 
 # several interfaces which is not what you want
 # setting packets per second to 10,000
@@ -46,7 +46,7 @@ grep 'Host' ./results/masscan.out | \
     tr '\n' ',' | \
     sed 's/,$//' > ./results/ports
 
-echo -e "Starting Nmap scan (\e[0;92mINFO\e[0m)"
+echo -e "\nStarting Nmap scan (\e[0;92mINFO\e[0m)\n"
 # run nmap against the target IP's in file, specifying the ports 
 # that were discovered port by masscan for further interegation
 nmap -v -iL $IP_FILE -p $(cat ./results/ports) -A -Pn -oA ./results/nmap-$(date '+%Y%m%d%M%S')-1
