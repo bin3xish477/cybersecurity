42daed19aea01d0fc07163e004cb9783bc45208e
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2021-09-29 18:00:48 -0400

    Add files via upload

--- Phishing/Gophish/user-data.sh
+++ Phishing/Gophish/user-data.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-mkdir /opt/gophish
-cd /opt/gophish
-wget -O gophish.zip https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
-unzip gophish.zip
-sed -i 's|127\.0\.0\.1|0\.0\.0\.0|g' config.json
-chmod +x ./gophish
-./gophish
+gophish_setup() {
+  mkdir /opt/gophish
+  cd /opt/gophish
+  wget -O gophish.zip https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
+  unzip gophish.zip
+  sed -i 's|127\.0\.0\.1|0\.0\.0\.0|g' config.json
+  chmod +x ./gophish
+  ./gophish &
+}
 
+mailhog_setup() {
+  # Use mailhog as the sending SMTP server
+  mkdir /opt/mailhog
+  cd /opt/mailhog
+  wget -O mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_amd64
+  chmod +x ./mailhog
+  ./mailhog &
+}
+
+gophish_setup
+mailhog_setup
