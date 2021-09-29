7ffd12db285c971edcfb883ffb23d2eecff699c8
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2021-09-29 13:51:19 -0400

    Update key_pair.tf

--- Phishing/Gophish/key_pair.tf
+++ Phishing/Gophish/key_pair.tf
@@ -1,3 +1,4 @@
+# NOTE: run the command: `ssh-keygen -f id_rsa` to generate private and public SSH keys
 resource "aws_key_pair" "gophish_ssh_key" {
   key_name = "gophish-ssh-key"
   public_key = "${file("id_rsa.pub")}"
