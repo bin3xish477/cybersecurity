7e8cccb4a3975ecde24484bf740080f689bfe84d
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-10-12 17:52:16 -0400

    adding config.json file to massbuster

--- MyTools/RedTeam/massbuster/massbuster.go
+++ MyTools/RedTeam/massbuster/massbuster.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/alexflint/go-arg"
 )
 
 var args struct {
-	File     string `arg:"-f,--file" help:"specify the path to file containing IPs/hostnames"`
+	HostFile string `arg:"-f,--file" help:"specify the path to file containing IPs/hostnames"`
 	Wordlist string `arg:"-w,--wordlist" help:"specify the path to wordlist"`
+	Threads  int    `arg:"-t,--threads" help:"number of threads to use" default:"10"`
+}
+
+type Config struct {
+	GobusterArgs string `json:"gobuster"`
+	HttpxArgs    string `json:"httpx"`
 }
 
 var (
 	hostFile string
 	wordList string
+	threads  int
+	config   *Config
+)
+
+const (
+	green  = "\u001b[32m"
+	end    = "\u001b[0m"
+	underL = "\u001b[4m"
 )
 
 func getCommandPath(cmd string) (string, bool) {
@@ -27,27 +45,26 @@ func getCommandPath(cmd string) (string, bool) {
 	return path, true
 }
 
-//type Command struct {
-//Shell string
-//Args  []string
-//Cmd   *exec.Cmd
-//}
+func parseConfig() {
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("[-] unable to read `config.json` file")
+		os.Exit(1)
+	}
+	defer jsonFile.Close()
 
-//func (c *Command) exec() {
-////fmt.Println(c.Shell, c.Args)
-//c.Cmd = exec.Command(c.Shell, c.Args...)
-//c.Cmd.Stdin = os.Stdin
-//c.Cmd.Stdout = os.Stdout
-//c.Cmd.Stderr = os.Stderr
-//c.Cmd.Run()
-//}
+	jsonBytes, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(jsonBytes, &config)
+}
 
-func massBust(host string) {
+func gobust(host string) {
+	fmt.Printf(
+		"URL: %s%s%s%s\n-----------------------------------------------------\n", green, underL, host, end,
+	)
+	mainArgs := fmt.Sprintf("-t %s -u %s -w %s", strconv.Itoa(threads), host, wordList)
+	allArgs := fmt.Sprintf("%s %s", mainArgs, config.GobusterArgs)
 	if gobusterPath, exists := getCommandPath("gobuster"); exists {
-		cmd := exec.Command(gobusterPath,
-			"-q", "--no-error", "--useragent", "noleak",
-			"-u", hostFile, "-w", wordList,
-		)
+		cmd := exec.Command(gobusterPath, strings.Split(allArgs, " ")...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -56,26 +73,25 @@ func massBust(host string) {
 		fmt.Println("[!] need to install gobuster.. make sure its in your PATH")
 		os.Exit(1)
 	}
-
+	fmt.Println()
 }
 
-func hostStatusCheck() {
+func start() {
 	if httpxPath, exists := getCommandPath("httpx"); exists {
 		cmd := exec.Command(httpxPath,
 			"-silent", "-random-agent", "-no-color",
-			"-list", hostFile,
+			"-threads", strconv.Itoa(threads), "-list", hostFile,
 		)
 
 		r, _ := cmd.StdoutPipe()
 
 		done := make(chan bool)
-
 		scanner := bufio.NewScanner(r)
 
 		go func() {
 			for scanner.Scan() {
 				host := scanner.Text()
-				massBust(host)
+				gobust(host)
 			}
 			done <- true
 		}()
@@ -99,8 +115,15 @@ func hostStatusCheck() {
 func main() {
 	arg.MustParse(&args)
 
-	hostFile = args.File
+	hostFile = args.HostFile
 	wordList = args.Wordlist
+	threads = args.Threads
 
-	hostStatusCheck(ipList)
+	if hostFile == "" || wordList == "" {
+		fmt.Println("error: must `-f` and `-w` arguments")
+		return
+	}
+
+	parseConfig()
+	start()
 }
