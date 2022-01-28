f3170bd2d895fe1c1ea9eac49b64a402e89d5d1b
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-01-27 20:17:33 -0500

    Update TaskTemplate.cs

--- MicrosoftAttacks/MSBuild/TaskTemplate.cs
+++ MicrosoftAttacks/MSBuild/TaskTemplate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -8,7 +9,7 @@ namespace MyTasks
   {
     public override bool Execute()
     {
-      return true;
+      Process.Start("calc.exe");
     }
   }
 }
