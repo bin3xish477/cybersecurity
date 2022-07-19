0364323e458c039fc57cd2fb98467d80e7fc1774
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-07-19 06:32:36 -0400

    Create account-takeover-register-function.js

---
+++ SecureCodeReview/code-samples/account-takeover-register-function.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { full_name, email, passwd } = res.body;
+  
+  if (!(full_name && email && passwd)) {
+    return res.json("missing fields");
+  }
+  
+  try {
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      return res.json({error: "user exists. redirecting to login"});
+    }
+    
+    passwdHash = await bcrypt.hash(passwd, 10);
+    const user = await User.create({
+      full_name: full_name,
+      email: email.toLowerCase(),
+      passwd: passwdHash
+    });
+    
+    return res.json({ full_name, email, createdAt: user.createdAt });
+    
+  } catch (err) {
+    return res.json(err);
+  }
+});
+
+module.exports = router;
