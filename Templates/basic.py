1760842ff22be118671e7915106f0cc7da551171
Alex Rodriguez <arodriguez99@protonmail.com>
2023-12-03 04:37:12 -0500

    Adds new directory, Templates, and a Python script for web app automation task

---
+++ Templates/basic.py
@@ -0,0 +1,74 @@
+"""this file contains a basic template for starting a new web app attack script"""
+
+from typing import Any
+import requests
+import json
+import base64
+
+requests.urllib3.disable_warnings()
+
+url = "http://localhost:8080"
+proxies = {
+    "http": "http://localhost:8080",
+    "https": "https://localhost:8080",
+}
+ok_codes = [200, 201, 204, 301, 304]
+
+
+def b64(s: str) -> str:
+    return base64.b64encode(bytes(s, encoding="utf8")).decode()
+
+
+def d64(s: str) -> str:
+    return base64.b64decode(s).decode(encoding="utf8")
+
+
+def json_to_dict(s: str) -> dict:
+    return json.loads(s)
+
+
+def ok(r: requests.Response) -> bool:
+    return r.status_code in ok_codes
+
+
+# send HTTP requests
+def hit(
+    method: str = "get",
+    url: str = "",
+    data: Any = None,
+    as_json: bool = False,
+    proxy: bool = False,
+    headers: dict = {},
+    verify: bool = False,
+    redirects: bool = True,
+) -> requests.Response:
+    if as_json:
+        headers["Content-Type"] = "application/json"
+    return requests.request(
+        method=method,
+        url=url,
+        data=json.dumps(data) if as_json else data,
+        headers=headers,
+        proxies=proxies if proxy else None,
+        verify=verify,
+        allow_redirects=redirects,
+    )
+
+
+def main():
+    # resp = hit(
+    #    method="post",
+    #    url=url,
+    #    data={"hello": "universe"},
+    #    as_json=True,
+    #    verify=False,
+    # )
+    resp = hit(url=url, verify=False)
+    if ok(resp):
+        pass
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
